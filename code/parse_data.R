f70f4833a3dd616b48788cc994a155c860e83690
Nikolai Myllymäki <nikolai@localhost.localdomain>
2016-07-15 15:19:14 -0700

    Remove single quotation marks (') from character columns

--- code/parse_data.R
+++ code/parse_data.R
@@ -2,7 +2,13 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(tidyr)
+
+NoQuotations <- function(x) {
+  str_replace_all(x, "\\'*", "")
+}
+
 y2013 <- read_csv("data/thads2013n.txt") %>%
-  setNames(str_to_lower(names(y2013))) %>%
-  mutate(control = str_replace_all(control, "\\'*", "")) %>%
-  head
+  setNames(str_to_lower(names(.))) %>%  
+  mutate_if(is.character, NoQuotations)
+  
+  
