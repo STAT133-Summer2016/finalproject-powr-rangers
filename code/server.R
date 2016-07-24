17448cd57456aa4a561ca9a105ae34f0c1943f85
Nikolai Myllymäki <nikolai.myllymaki@gmail.com>
2016-07-24 14:21:26 -0700

    Add comments to ui and server code

--- code/server.R
+++ code/server.R
@@ -4,11 +4,16 @@ library(readr)
 library(maps)
 library(ggmap)
 
+# dirty_sample is just a sample of the full combined but unmodified dataset,
+# saved to a separate file for convenience:
+# sample_n(dirty_dataset, 10000)
 dirty_sample <- read_csv("../data/dirty_sample.csv")
 
+# Cleaning the data
 clean_sample <- dirty_sample %>% 
   setNames(str_to_lower(names(.))) %>%  
   mutate_if(is.character, NoQuotations) %>%  
+  # -9 has been used as a marker for NA, replace them with NA
   na_if(-9) %>%
   mutate(ownrent = factor(ownrent,   
                           levels = c("1", "2"), 
@@ -19,10 +24,12 @@ shinyServer(function(input, output) {
   output$plot <- renderPlot({ 
     
      clean_sample %>%  
+      # Use NSE
       group_by_("dataset_year", input$sel_var) %>%  
       # Take the median instead of mean to avoid extreme observations skewing the 
-      # result. 
+      # result.
       summarise(burden_year = median(burden, na.rm = T)) %>%  
+      # Use NSE
       ggplot(aes_string(x = "dataset_year",   
                         y = "burden_year",   
                         colour = input$sel_var)) +  
--- code/ui.R
+++ code/ui.R
@@ -1,13 +1,15 @@
 shinyUI(fluidPage(
 
-  titlePanel("Dialects"),
+  titlePanel("Burden by year and category"),
   
   sidebarLayout(
     sidebarPanel(
       
       selectInput("sel_var",
-                  label = "Variable",
+                  label = "Grouping variable",
                   choices = names(clean_sample),
+                  # Select "ownrent" as initial value to avoid getting a 
+                  # non-categorical variable by default
                   selected = "ownrent")
       
       ),
