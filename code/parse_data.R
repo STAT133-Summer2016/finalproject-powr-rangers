5e0ecad417bb36e482b318765b1c2ef694c420e8
SriKondapally <hensels@berkeley.edu>
2016-07-25 10:08:21 -0700

    code

--- code/parse_data.R
+++ code/parse_data.R
@@ -7,8 +7,17 @@ NoQuotations <- function(x) {
   str_replace_all(x, "^\\'|\\'$", "")
 }
 
-y2013 <- read_csv("data/thads2013n.txt") %>%
+clean <- modernfiles %>%
+  na_if(., -9) %>%
   setNames(str_to_lower(names(.))) %>%  
   mutate_if(is.character, NoQuotations)
+
+#plot
+clean %>% 
+  group_by(dataset_year) %>% 
+  summarise(burden_year = mean(burden)) %>% 
+  ggplot(aes(x = dataset_year, y = burden_year)) + 
+  geom_line()
   
+ 
   
