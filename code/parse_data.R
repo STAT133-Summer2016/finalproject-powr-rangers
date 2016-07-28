e1374b5556865bd119b252da908d3ef1ca333751
Nikolai Myllymäki <nikolai.myllymaki@gmail.com>
2016-07-27 18:46:32 -0700

    Add manual column types to avoid parsing failures

--- code/parse_data.R
+++ code/parse_data.R
@@ -3,10 +3,9 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 
-# dirty_sample is just a sample of the full combined but unmodified dataset,
-# saved to a separate file for convenience:
-combined_years <- read_csv("../data/combined_years.csv")
-
+combined_years <- read_csv("../data/combined_years.csv",
+                           # Add col_types manually to avoid parsing failures
+                           col_types = "cicciiciiciiidicidicddddddiidddddddddddddddddididididididididididididididicccccccccccccccccccccccccd")
 
 NoQuotations <- function(x) {
   str_replace_all(x, "^\\'|\\'$", "")
@@ -17,14 +16,20 @@ clean_years <- combined_years %>%
   setNames(str_to_lower(names(.))) %>%  
   mutate_if(is.character, NoQuotations) %>%  
   # -9 has been used as a marker for NA, replace them with NA
-  na_if(-9) %>%
+  na_if(-9)  %>%
   mutate(ownrent = factor(ownrent,   
                           levels = c("1", "2"), 
                           labels = c("own", "rent")))
 
-set.seed(1337)  
-clean_sample <- sample_n(clean_years, 100000)
+  clean_sample <- mutate(clean_sample, ownrent = factor(ownrent,   
+                          levels = c("1", "2"), 
+                          labels = c("own", "rent")))
+# Free some memory
+remove(combined_years)
 
-#write csv files
+set.seed(1337)  
+clean_sample <- sample_n(clean_years, 10000)
+
+# Write csv files
 write_csv(clean_years, path = "../data/clean_years.csv")
 write_csv(clean_sample, path = "../data/clean_sample.csv")
