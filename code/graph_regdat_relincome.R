31cbd8c12eed27c87471296e07c6a7183d3463ab
SriKondapally <hensels@berkeley.edu>
2016-07-30 14:06:25 -0700

    APL median by region code

---
+++ code/graph_regdat_relincome.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(readr)
+library(stringr)
+library(dplyr)
+col_types = "cicciiciiciiidicidicddddddiidddddddddddddddddididididididididididididididicccccccccccccccccccccccccd"
+
+clean_years <- read_csv("../data/clean_years.csv", col_types = col)
+
+clean_years %>%
+  group_by(year, region) %>%  
+  # Take the median instead of mean to avoid extreme observations skewing the 
+  # result.
+  summarise(new_y = median(aplmed, na.rm = T)) %>%
+  ggplot(aes(x = year,   
+                  y = new_y,   
+                  colour = region)) + geom_line() 
