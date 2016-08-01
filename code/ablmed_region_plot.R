54c24d8bf3e84f80e3dd8df8d60b7e2fabac76d8
SriKondapally <hensels@berkeley.edu>
2016-08-01 12:32:52 -0700

    update plot codes

--- code/ablmed_region_plot.R
+++ code/ablmed_region_plot.R
@@ -7,11 +7,11 @@ library(dplyr)
 clean_years <- read_csv("../data/clean_years.csv") #col_types = col)
 
 clean_years %>%
-  group_by(dataset_year, region) %>%  
+  group_by(year, region) %>%  
   # Take the median instead of mean to avoid extreme observations skewing the 
   # result.
   summarise(new_y = median(ablmed, na.rm = T)) %>%
-  ggplot(aes(x = dataset_year,   
+  ggplot(aes(x = year,   
              y = new_y,   
              colour = region)) + geom_line() +
   labs(x = "year", y = "Median Adjusted Income (per persons)", 
